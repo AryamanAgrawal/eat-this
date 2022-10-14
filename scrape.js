d6d8739489b1e37e0bb9d076381d2a0653539ca5
Elvira Rui Xiong <47922282+rxiong19@users.noreply.github.com>
2022-10-14 17:26:37 -0400

    fix JSON

--- scrape.js
+++ scrape.js
@@ -7,21 +7,22 @@ const cron = require('node-cron');
 
 (async () => {
     let url = 'https://umassdining.com/locations-menus/berkshire/menu';
-    cron.schedule('10 * * * * *', async() => {
+    //cron.schedule('* * * * * *', async() => {
         let response = await rp(url);
         let $ = cheerio.load(response);
         let count = $('a').length;
         for (let i = 0; i< count; i++){
             let test = $('a[data-ingredient-list]')[i.toString()];
             if (test !== undefined){
-                let name = test['attribs']['data-dish-name'].toString();
+                let dishname = test['attribs']['data-dish-name'].toString();
                 let ingredient = test['attribs']['data-ingredient-list'].toString();
-                let dish = {name : ingredient};
+                let dish = {};
+                dish[dishname] = ingredient;
                 let dishJSON = JSON.stringify(dish);
                 console.log(dishJSON + "\n");
             }
         }
-    });
+    //});
 
 })();
 
