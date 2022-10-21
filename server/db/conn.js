a8961fad98c2b9e962424bbd7355d1aa86a6363e
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-10-20 23:00:11 -0400

    Revert "Initial commit"
    This reverts commit 203ec300959896dad664ff1ca0ad93e908ef709e.

--- server/db/conn.js
+++ server/db/conn.js
@@ -12,7 +12,7 @@ module.exports = {
         client.connect(function (err, db) {
             // Verify we got a good "db" object
             if (db) {
-                _db = db.db("eatThis");
+                _db = db.db("employees");
                 console.log("Successfully connected to MongoDB.");
             }
             return callback(err);
