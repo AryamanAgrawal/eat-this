b2834eb3dc07757139738c910566d7f7e8ff4dbd
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-10-20 23:49:52 -0400

    login update

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+app.use(require("./routes/user"));
 // get driver connection
 const dbo = require("./db/conn");
 
