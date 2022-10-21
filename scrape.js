52d3948a490b21ddab1f9887c1bbe3233264dc7b
Elvira Rui Xiong <47922282+rxiong19@users.noreply.github.com>
2022-10-21 17:24:39 -0400

    update 4 DCs

--- scrape.js
+++ scrape.js
@@ -1,30 +1,36 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const cron = require('node-cron');
 
-(async () => {
-    let url = 'https://umassdining.com/locations-menus/berkshire/menu';
-    cron.schedule('* * * * * *', async() => {
-        let response = await rp(url);
-        let $ = cheerio.load(response);
-        let count = $('a').length;
-        for (let i = 0; i< count; i++){
-            let test = $('a[data-ingredient-list]')[i.toString()];
-            if (test !== undefined){
-                let dishname = test['attribs']['data-dish-name'].toString();
-                let ingredient = test['attribs']['data-ingredient-list'].toString();
-                let dish = {};
-                dish[dishname] = ingredient;
-                let dishJSON = JSON.stringify(dish);
-                console.log(dishJSON + "\n");
-            }
-        }
-    });
+const dining = ['berkshire','hampshire','worcester','franklin'];
 
+(async () => {
+    
+    dining.map(async(location) => {
+        
+        let url = 'https://umassdining.com/locations-menus/'+location+'/menu';
+        //cron.schedule('30 * * * * *', async() => {
+            let response = await rp(url);
+            let $ = cheerio.load(response);
+            let count = $('a').length;
+            console.log("---------------------------"+location+"------------------------------"+"\n");
+            for (let i = 0; i< count; i++){
+                let test = $('a[data-ingredient-list]')[i.toString()];
+                if (test !== undefined){
+                    let dishname = test['attribs']['data-dish-name'].toString();
+                    let ingredient = test['attribs']['data-ingredient-list'].toString();
+                    let dish = {};
+                    dish[dishname] = ingredient;
+                    let dishJSON = JSON.stringify(dish);
+                    console.log(location+":  "+ dishJSON + "\n");
+                }
+            }
+        //});
+    });
 })();
 
 
-module.exports = router;
+// module.exports = router;
