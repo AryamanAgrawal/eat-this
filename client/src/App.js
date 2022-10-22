880d1b11c916e34676ecb8d786fa6074d90430ff
YuQian Shi <yuqianshi@umass.edu>
2022-10-22 11:25:27 -0400

    some change

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,13 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import DiningCard from "./components/dinningCard";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import cardData from "./cardData";
+// import "./components/cardindex.css"
+
 
 const App = () => {
     return (
@@ -18,8 +25,23 @@ const App = () => {
                 <Route path="/edit/:id" element={<Edit />} />
                 <Route path="/create" element={<Create />} />
             </Routes>
+            <div className="home-cards">
+                <DiningCard />
+                {/* <DiningCard deatails={cardData} /> */}
+                {/* <Row xs={1} md={2} className="g-4">
+                {Array.from({ length: 3 }).map((_, idx) => (
+                    <Col>
+                        <DiningCard deatails={cardData} />
+                    </Col>
+                ))}
+                </Row> */}
+            </div>
+            
         </div>
+
+        
     );
 };
 
 export default App;
+
