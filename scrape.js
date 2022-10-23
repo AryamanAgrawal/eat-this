f32361af04db45928661512a1a8f993a2e2a686a
Elvira Rui Xiong <47922282+rxiong19@users.noreply.github.com>
2022-10-22 23:29:14 -0400

    update data format, add mealtime, and generate JSON file

--- scrape.js
+++ scrape.js
@@ -5,44 +5,62 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const cron = require('node-cron');
 
-const dining = ['berkshire','hampshire','worcester','franklin'];
+const fs = require('fs');
 
-(async () => {
+const dining = ['berkshire','hampshire','worcester','franklin'];
+const mealtime = ['#breakfast_men','#lunch_menu','#dinner_menu','#latenight_menu','#grabngo'];
+
+
+
     
-    dining.map(async(location) => {
+(async() => { 
+    let menu = {};
+    let dc_time = '';
+        dining.map(async(location) => {
         
-        let url = 'https://umassdining.com/locations-menus/'+location+'/menu';
-        //cron.schedule('0 1 * * *', async() => {
+        let url = 'https://umassdining.com/locations-menus/'+location+'/menu'; 
+        //cron.schedule('0 1 * * *', async() => { //disable for now
             let response = await rp(url);
             let $ = cheerio.load(response);
-            let count = $('a').length;
-            console.log("---------------------------"+location+"------------------------------"+"\n");
-            for (let i = 0; i< count; i++){
-                let test = $('a[data-ingredient-list]')[i.toString()];
-                if (test !== undefined){
-                    //dishname: ingredient, allergens, nutritionValues, cusineType(?)
-                    let dishname = test['attribs']['data-dish-name'].toString();
-                    let ingredient = test['attribs']['data-ingredient-list'].toString();
-                    let allergens = test['attribs']['data-allergens'].toString();
+            
+            //console.log("---------------------------------"+location+"----------------------------------"+"\n");
+            let dishlist = [];
+            mealtime.map(async(t) => {
+                if($(t).html() !== null){
+                    //console.log("----------------------------------------------"+t+"----------------------------------------------");
+                    $(t).find('div > li > a').each((index, elem)=>{
+                        dc_time = location +t;
+                        let dish_attributes = $(elem)['0']['attribs'];
+                        let dish = {}; // {dishname: [ingredient, allergens, nutritionValues]}
+                        let dishname = dish_attributes['data-dish-name'].toString();
+                        let ingredient = dish_attributes['data-ingredient-list'].toString();
+                        let allergens = dish_attributes['data-allergens'].toString();  
+                        let nutritionValues = [
+                            dish_attributes['data-calories'].toString(),
+                            dish_attributes['data-total-fat'].toString(),
+                            dish_attributes['data-cholesterol'].toString(),
+                            dish_attributes['data-sodium'].toString(),
+                            dish_attributes['data-total-carb'].toString(),
+                            dish_attributes['data-dietary-fiber'].toString(),
+                            dish_attributes['data-sugars'].toString(),
+                            dish_attributes['data-protein'].toString()
+                        ];   
+                        dish[dishname] = [ingredient, allergens, nutritionValues];
+                        let dishJSON = JSON.stringify(dish);
+                        dishlist.push(dishJSON);
+                    });
+                    menu[dc_time] = dishlist; 
+                    //{worcester#lunch_menu:[{dishname: [ingredient, allergens, nutritionValues]},
+                    //                       {dishname: [ingredient, allergens, nutritionValues]},...]}
                     
-                    let nutritionValues = [
-                        test['attribs']['data-calories'].toString(),
-                        test['attribs']['data-total-fat'].toString(),
-                        test['attribs']['data-cholesterol'].toString(),
-                        test['attribs']['data-sodium'].toString(),
-                        test['attribs']['data-total-carb'].toString(),
-                        test['attribs']['data-dietary-fiber'].toString(),
-                        test['attribs']['data-sugars'].toString(),
-                        test['attribs']['data-protein'].toString()
-                    ];
-                    let dish = {};
-                    dish[dishname] = [ingredient, allergens, nutritionValues];
-                    let dishJSON = JSON.stringify(dish);
-                    console.log(location+":  "+ dishJSON + "\n");
                 }
-            }
+            });
+            
         //});
+        console.log(menu);
+        fs.writeFileSync("dishdata.json", JSON.stringify(menu));
     });
+    
 })();
 
 
