3808b164112e899fe941a0d29ff59a7733c57639
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-10-23 16:12:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-
 app.use(express.static(path.resolve(__dirname, "./client/build")));
-
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 8000;
 app.use(cors());
