2257e19e49b39910d223b2f8cf16618fdf715d9d
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-10-23 16:05:24 -0400

    updating file structure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 8000;
@@ -18,7 +18,6 @@ app.listen(port, () => {
     // perform a database connection when server starts
     dbo.connectToServer(function (err) {
         if (err) console.error(err);
-
     });
     console.log(`Server is running on port: ${port}`);
 });
