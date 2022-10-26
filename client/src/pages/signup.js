0a086c40e9975fa5510b64d853c020243114744d
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-10-25 22:25:19 -0400

    Update signup.js

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -20,7 +20,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8000/signup", {
+      const response = await fetch("http://localhost:8000/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
