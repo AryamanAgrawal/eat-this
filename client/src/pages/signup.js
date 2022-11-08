b1a9cae2e3a6060aef87974cd04fd44c0531b75f
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-11-08 00:30:44 -0500

    Update routes

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -23,7 +23,7 @@ const Signup = () => {
     setLoading(true);
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8000/register", {
+      const response = await fetch("https://umasseatthis.herokuapp.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
