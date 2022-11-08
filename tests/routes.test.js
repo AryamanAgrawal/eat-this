cb339f8a81edfcefdaf020bdd3e5179e8390cd82
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-11-08 00:48:35 -0500

    Update routes.test.js

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,21 +2,7 @@ const { ObjectId } = require('bson');
 const request = require('supertest')
 url = "https://umasseatthis.herokuapp.com";
 
-describe('test /resgister endpoint', () => {
-    it('should create a new post', async () => {
-        const response = await request(url)
-            .post('/register')
-            .send({
-                firstName: 'Alex',
-                lastName: 'Dhima',
-                email: 'adhima@mail.com',
-                password: '1234'
-            })
-        expect(response.statusCode).toEqual(200);
-    })
-})
-
-describe('test /resgister endpoint with duplicate user', () => {
+describe('test /register endpoint with duplicate user', () => {
     it('should not create a new post', async () => {
         const response = await request(url)
             .post('/register')
