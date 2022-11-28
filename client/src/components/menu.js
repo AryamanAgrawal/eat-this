b9712fd286979573a0cbf22b170df70641a44e67
YuQian Shi <yuqianshi@umass.edu>
2022-11-28 00:51:29 -0500

    fix fetch

--- client/src/components/menu.js
+++ client/src/components/menu.js
@@ -9,13 +9,14 @@ function Menu({diningLocationId}){
         const fetchData = async () => {
             // return menu object for the desired dinning commons
             console.log(diningLocationId);
-            const response = await fetch(`https://umasseatthis.herokuapp.com/menu/:${diningLocationId}`); 
+            const response = await fetch(`https://umasseatthis.herokuapp.com/menu/${diningLocationId}`); 
 
             if (!response.ok) {
                 const message = `An error occurred: ${response.statusText}`;
                 window.alert(message);
                 return;
             }
+            console.log("okok")
 
             const records = await response.json();
             setMenuData(records.results); //store foodIds array
@@ -28,7 +29,7 @@ function Menu({diningLocationId}){
     useEffect(() => {
         const fetchData = async () => {
             menuData.forEach(async foodId => {
-                const response = await fetch(`https://umasseatthis.herokuapp.com/menu/:${foodId}`); // return menu object
+                const response = await fetch(`https://umasseatthis.herokuapp.com/menu/${foodId}`); // return menu object
 
                 if (!response.ok) {
                     const message = `An error occurred: ${response.statusText}`;
