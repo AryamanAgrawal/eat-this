2291aa822c9cfd3f6f8fed5a97ea2aa0267580c1
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-12-04 22:19:29 -0500

    Update login.js

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -79,7 +79,7 @@ const Login = () => {
         </div>
 
         <div className={styles.right}>
-          <h1>New Here ?</h1>
+          <h1>New Here?</h1>
           <Link to="/signup">
             <button type="button" className={styles.white_btn}>
               Sign Up
